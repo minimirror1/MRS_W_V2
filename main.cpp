d8818faba05c62f9909c9d70481e429c70674397
minimirror1 <minimirror1@gmail.com>
2024-10-11 11:06:35 +0900

    목록 비활성화 색상 변경

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
     // Fusion 스타일 사용
-    a.setStyle(QStyleFactory::create("Fusion"));
+    //a.setStyle(QStyleFactory::create("Fusion"));
 
     // 밝은 팔레트를 사용하여 라이트 모드 보장
     QPalette lightPalette;
@@ -17,6 +17,11 @@ int main(int argc, char *argv[]) {
     lightPalette.setColor(QPalette::Text, QColor(0, 0, 0));         // 입력 텍스트 색상
     lightPalette.setColor(QPalette::Button, QColor(240, 240, 240)); // 버튼 배경색
     lightPalette.setColor(QPalette::ButtonText, QColor(0, 0, 0));   // 버튼 텍스트 색상
+
+    // 비활성화 상태에 대한 색상 설정 추가
+    lightPalette.setColor(QPalette::Disabled, QPalette::WindowText, QColor(128, 128, 128)); // 비활성화된 텍스트 색상
+    lightPalette.setColor(QPalette::Disabled, QPalette::Text, QColor(128, 128, 128));       // 입력 위젯의 비활성화된 텍스트 색상
+    lightPalette.setColor(QPalette::Disabled, QPalette::ButtonText, QColor(128, 128, 128)); // 버튼의 비활성화된 텍스트 색상
 
     a.setPalette(lightPalette);
 
